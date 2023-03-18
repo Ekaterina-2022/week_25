f45b3d52bdc3b1b707c8f01cde3983c62169c3a5
Ekaterina-2022 <davydovakaterina2022@gmail.com>
2023-03-18 23:31:43 +0300

    docs:new project

---
+++ my-app/src/App.js
@@ -0,0 +1,44 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const data = {
+	labels: ["English", "Chinese", "Hindi", "Spanish", "Arabian", "Bengali"],
+	datasets: [
+		{
+			label: "Top 5 the most popular world languages (% by population)",
+			data: [17, 14, 8, 7, 3, 3],
+			backgroundColor: [
+				"rgba(255, 99, 132, 0.2)",
+				"rgba(54, 162, 235, 0.2)",
+				"rgba(255, 206, 86, 0.2)",
+				"rgba(75, 192, 192, 0.2)",
+				"rgba(153, 102, 255, 0.2)",
+				"rgba(255, 159, 64, 0.2)",
+			],
+			borderColor: [
+				"rgba(255, 99, 132, 1)",
+				"rgba(54, 162, 235, 1)",
+				"rgba(255, 206, 86, 1)",
+				"rgba(75, 192, 192, 1)",
+				"rgba(153, 102, 255, 1)",
+				"rgba(255, 159, 64, 1)",
+			],
+			borderWidth: 1,
+		},
+	],
+};
+
+function App() {
+	return <Pie data={data} />;
+}
+
+/*function App() {
+	return <div className="App">hello</div>;
+}
+*/
+export default App;
